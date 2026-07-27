241839fb25c4602289369ad990d59c2e3ca5e169
KRISH Pandya <krishpandya520@gmail.com>
2026-07-27 14:15:32 +0530

    Refactored load_data.py using utils module

--- python/load_data.py
+++ python/load_data.py
@@ -1,28 +1,33 @@
 import pandas as pd
-from pathlib import Path
 
-# Project Root Folder
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+from utils import RAW_DATA
 
-# Raw Data Folder
-DATA_PATH = PROJECT_ROOT / "data" / "raw"
+# ======================================
+# Load Data
+# ======================================
 
-# Load CSV Files
-accounts = pd.read_csv(DATA_PATH / "accounts.csv")
-products = pd.read_csv(DATA_PATH / "products.csv")
-sales_pipeline = pd.read_csv(DATA_PATH / "sales_pipeline.csv")
-sales_teams = pd.read_csv(DATA_PATH / "sales_teams.csv")
+accounts = pd.read_csv(RAW_DATA / "accounts.csv")
+products = pd.read_csv(RAW_DATA / "products.csv")
+sales_pipeline = pd.read_csv(RAW_DATA / "sales_pipeline.csv")
+sales_teams = pd.read_csv(RAW_DATA / "sales_teams.csv")
 
-# Function to Display Dataset Information
-def dataset_info(df, name):
-    print("=" * 60)
-    print(f"{name.upper()} DATASET")
+datasets = {
+    "Accounts": accounts,
+    "Products": products,
+    "Sales Pipeline": sales_pipeline,
+    "Sales Teams": sales_teams
+}
+
+for name, df in datasets.items():
+
+    print("\n" + "=" * 60)
+    print(name.upper())
     print("=" * 60)
 
     print("\nFirst 5 Rows")
     print(df.head())
 
-    print("\nDataset Shape")
+    print("\nShape")
     print(df.shape)
 
     print("\nColumns")
@@ -36,33 +41,3 @@ def dataset_info(df, name):
 
     print("\nDuplicate Rows")
     print(df.duplicated().sum())
-
-    print("\n")
-
-
-# Display Information
-dataset_info(accounts, "Accounts")
-dataset_info(products, "Products")
-dataset_info(sales_pipeline, "Sales Pipeline")
-dataset_info(sales_teams, "Sales Teams")
-
-print("=" * 60)
-print("All datasets loaded successfully.")
-print("=" * 60)
-
-print("\n========== DEAL STAGE COUNTS ==========\n")
-print(sales_pipeline["deal_stage"].value_counts())
-
-print("\n========== MISSING CLOSE DATE BY DEAL STAGE ==========\n")
-print(
-    sales_pipeline[sales_pipeline["close_date"].isna()]
-    ["deal_stage"]
-    .value_counts()
-)
-
-print("\n========== MISSING CLOSE VALUE BY DEAL STAGE ==========\n")
-print(
-    sales_pipeline[sales_pipeline["close_value"].isna()]
-    ["deal_stage"]
-    .value_counts()
-)
